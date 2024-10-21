c9e1e849ad2c948599dfdfea406cb4a7615c98f8
quimlr <quimlagunas@gmail.com>
2024-10-21 03:53:38 +0200

    Added 'delete account' endpoint

--- UBReads_backend/app/auth.py
+++ UBReads_backend/app/auth.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from . import schemas, crud
 from .dependencies import get_current_user, get_db
 from .models import User
+from .schemas import UserResponse
 from .security import create_access_token, verify_password
 
 router = APIRouter()
@@ -32,3 +33,17 @@ def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
 @router.get("/users/me", response_model=schemas.UserResponse)
 def get_current_user_info(current_user: User = Depends(get_current_user)):
     return current_user
+
+@router.delete("/delete-account/")
+def delete_account(db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
+    # Get the current user from the database
+    user = db.query(User).filter(User.email == current_user.email).first()
+    
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    # Delete the user from the database
+    db.delete(user)
+    db.commit()
+
+    return {"message": "User account deleted successfully"}
