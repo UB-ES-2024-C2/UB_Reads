4d001b444e6ac55043b2c08cf7d7dc8c92951750
subiranet <joan.joan425@gmail.com>
2024-10-22 18:49:38 +0200

    **Added Files:**
    - signup: front-end form (based on login)
    
    **Modified Files:**
    - App, login: allowing the navigation between the login and signup.

--- UBReads_frontend/src/signup.jsx
+++ UBReads_frontend/src/signup.jsx
@@ -1,8 +1,8 @@
-import "./styles/login.css";
+import "./styles/signup.css";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom"; // Importa useNavigate
 
-export const Login = () => {
+export const SignupForm = () => {
   const navigate = useNavigate(); // Hook para redirigir
 
   const handleSubmit = (e) => {
@@ -20,25 +20,32 @@ export const Login = () => {
         <div className="welcome-container">
           <h2>Benvingut a UBReads!</h2>
           <p>
-            Ens alegrem de tenir-te aquí. Siusplau, inicia sessió per accedir al
-            teu compte.
+            Ens alegrem de tenir-te aquí. Siusplau, crea un nou compte per poder accedir a <b>UB Reads</b>
           </p>
         </div>
         <div className="login-container">
           {/* Contenedor para el formulario */}
-          <h2>Login</h2>
+          <h2>Signup</h2>
           <form onSubmit={handleSubmit}>
             {" "}
             {/* Vincular la función al evento onSubmit */}
             <label htmlFor="username">Nom d'usuari:</label>
-            <input type="text" id="username" required />
+            <input type="text" id="username" required/>
+
+            <label htmlFor="email">Introdueix un Email</label>
+            <input type="email" id="email" required/>
+
             <label htmlFor="password">Contrasenya:</label>
-            <input type="password" id="password" required />
-            <button type="submit">Envia</button> {/* Botón para enviar */}
+            <input type="password" id="password" required/>
+
+            <label htmlFor="password2">Repeteix la contrasenya:</label>
+            <input type="password" id="password2" required/>
+
+            <button type="submit">Envia</button>
+            {/* Botón para enviar */}
           </form>
           <p>
-            No tens un compte? <Link to="/signup">Registra't aquí</Link>{" "}
-            {/* Link al registro */}
+            Ja tens un compte? <Link to="/">Logueja't aquí</Link>
           </p>
         </div>
       </div>
