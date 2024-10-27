16f858e4f0b5d8b9f22a20c9271f91be43f91d53
Andrei Demeter <andreidemeter@MacBook-Air-de-Andrei-2.local>
2024-10-27 10:06:26 +0100

    Database + cambis de archius i carpetas

---
+++ UBReads_backend/app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class UserBase(BaseModel):
+    
+    username: str
+    email: str
+
+class UserCreate(UserBase):
+     password: str
+
+class UserUpdate(UserBase):
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
--- UBReads_backend/main.py
+++ UBReads_backend/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.database import create_tables
+from app.routers.user_router import router as user_router
 
 app = FastAPI()
+
+create_tables()
+
+app.include_router(user_router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
 
 # Allow requests from the frontend (port 3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
