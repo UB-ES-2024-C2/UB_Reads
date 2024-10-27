4397d373b9cf09f21e8dbafa3c7b62d6c09a15e9
Johnny Astudillo Soriano <johnnyastudillosoriano@Johnnys-Air.technicolor.net>
2024-10-27 17:35:33 +0100

    Basic Home component created

---
+++ UBReads_frontend/src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/home.css';
+
+/**
+ * 
+ * @returns Home page with an empty div
+ */
+export const Home = () => {
+    return (
+        <div className="home-container">
+            <p>WELCOME TO UB READS</p>
+        </div>
+    )
+}
