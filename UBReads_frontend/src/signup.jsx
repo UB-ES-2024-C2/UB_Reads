ad83997b76264aeb8e01692b3113aecae5d7f626
Xevi <xevimarq@gmail.com>
2024-10-28 20:16:02 +0100

    minor_error fixed

--- UBReads_frontend/src/signup.jsx
+++ UBReads_frontend/src/signup.jsx
@@ -58,7 +58,7 @@ export const SignupForm = () => {
 
     if (!emailErrorMsg && !passwordErrorMsg && !confirmPasswordErrorMsg) {
       // Submit form logic (e.g., send data to server)
-      navigate("/login");
+      navigate("/");
     } else {
       setUnameError(uNameErorrMsg);
       setEmailError(emailErrorMsg);
