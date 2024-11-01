69819203d962ade3ed5bc31b0c9c06d6ffbd90b2
subiranet <joan.joan425@gmail.com>
2024-11-01 19:12:52 +0100

    Navbar finished

--- UBReads_frontend/src/navbar.jsx
+++ UBReads_frontend/src/navbar.jsx
@@ -1,5 +1,5 @@
 // React imports
-import React, { useEffect} from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link, useNavigate } from "react-router-dom"; // Router Link
 
 /* Material UI imports */
@@ -30,40 +30,42 @@ import Lupa from './assets/lupa.png'
  * @returns basic navigation bar with a simple user menu
  */
 export const NavBar = () => {
-
-  const navigate = useNavigate(); // Hook para redirigir
-
+  const navigate = useNavigate();
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const searchInputRef = useRef();
+
+  const handleSearch = async (e) => {
+      e.preventDefault();
+      const query = searchInputRef.current.value.trim();
+      alert("Searching query:\n" + query)
+  };
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
+    setAnchorElUser(null);
   };
 
-  /**
-   * Simple Log Out method implementation
-   */
   const logOut = () => {
-    navigate("/")
-  }
+    navigate("/");
+  };
 
   const profilePage = () => {
-      alert('Aixo hauria de navegar a la user page.\nNot implemented.')
-  }
+    alert('Aixo hauria de navegar a la user page.\nNot implemented.');
+  };
 
   useEffect(() => {
-      const handleClickOutside = (event) => {
-          if (anchorElUser && !anchorElUser.contains(event.target)) {
-              handleCloseUserMenu();
-          }
-      };
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-          document.removeEventListener("mousedown", handleClickOutside);
-      };
+    const handleClickOutside = (event) => {
+      if (anchorElUser && !anchorElUser.contains(event.target)) {
+        handleCloseUserMenu();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, [anchorElUser]);
 
   return (
@@ -72,42 +74,48 @@ export const NavBar = () => {
             <Link to="/home">
                 <Box sx={{ display: "flex", alignItems: "center" }}>
                     {/* Logo */}
-                    <img src={ Logo } alt="Logo" />
+                    <img src={Logo} alt="Logo" className="Logo"/>
                 </Box>
             </Link>
             {/* Searchbox */}
-            <Box className="search-container">
+            <form className="search-container" onSubmit={handleSearch}>
                 <Box className="search-box">
-                    <img src={ Lupa } alt="Lupa" className="serch-icon"/>
-                    <input type="text" className="search-bar" placeholder="Search..." />
+                    <img src={Lupa} alt="Lupa" className="search-icon"/>
+                    <input type="text" className="search-bar" ref={searchInputRef} />
                 </Box>
-            </Box>
+                <button type="submit" className="submit-btn">Cerca</button>
+            </form>
 
             <Box className="navbar-item">
                 {/* Avatar */}
                 <Tooltip title="User menu">
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                         <Avatar src="/broken-image.jpg"/>
+                        <Box className="user-data">
+                            <Typography className="user-text">Username</Typography>
+                            <Typography className="user-text">usermail@example.com</Typography>
+                        </Box>
                     </IconButton>
                 </Tooltip>
                 {/* User menu */}
                 <Menu
                     anchorEl={anchorElUser}
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}
-                    keepMounted transformOrigin={{ vertical: 'top', horizontal: 'center', }}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                    keepMounted
+                    transformOrigin={{ vertical: 'top', horizontal: 'center' }}
                     open={Boolean(anchorElUser)}
-                    onClose={null}
-                    sx={{ mt: "0.5rem" }} >
-
+                    onClose={handleCloseUserMenu}
+                    sx={{ mt: "0.5rem", minWidth: "13vw" }}
+                >
                     <MenuItem onClick={profilePage}>
-                        <Typography sx={{ textAlign: 'center', color: blue }}>Perfil</Typography>
+                        <Typography sx={{ textAlign: 'center', color: blue, minWidth: "13vw" }}>Perfil</Typography>
                     </MenuItem>
                     <MenuItem onClick={logOut}>
-                        <Typography sx={{ textAlign: 'center', color: pink[500] }}>Log Out</Typography>
+                        <Typography sx={{ textAlign: 'center', color: pink[500], minWidth: "13vw" }}>Log Out</Typography>
                     </MenuItem>
                 </Menu>
             </Box>
         </Stack>
     </Container>
   );
-}
+};
