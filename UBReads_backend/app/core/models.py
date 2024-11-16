3f01142e7e08f816567ea6f6c7a2628c69310e17
Andrei Demeter <andreidemeter@MacBook-Air-de-Andrei-2.local>
2024-11-16 16:18:53 +0100

    Arreglar endpoints que no funcionaban

--- UBReads_backend/app/core/models.py
+++ UBReads_backend/app/core/models.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Boolean
 from sqlalchemy.orm import relationship
-from .database import Base
+from .database import Base, engine
 
 # Association table to link users and books
 book_users = Table(
     "user_books",
     Base.metadata,
     Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
-    Column("book_id", Integer, ForeignKey("books.id"), primary_key=True)
+    Column("book_id", Integer, ForeignKey("books.id"), primary_key=True),
+    Column('is_read', Boolean)
 )
 
 class User(Base):
@@ -16,7 +17,7 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-
+    refresh_token = Column(String(255))
     # Many-to-many relationship: a user can own multiple books, and a book can be owned by multiple users
     books = relationship("Book", secondary= book_users, back_populates="owners")
 
@@ -24,9 +25,25 @@ class Book(Base):
     __tablename__ = "books"
     
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author = Column(String)
+    title = Column(String(255))
+    author = Column(String(255))
+    description = Column(String(120))
+    category = Column(String(50))
+    year = Column(Integer)
+    cover_url = Column(String(255))  
     
     # Many-to-many relationship: a book can have multiple owners (users)
     owners = relationship("User", secondary= book_users, back_populates="books")
 
+def create_tables():
+    Base.metadata.create_all(engine)
+
+def reset_database():
+    # Elimina todas las tablas
+    print("Eliminando todas las tablas...")
+    Base.metadata.drop_all(bind=engine)
+    
+    # Crea las tablas nuevamente según los modelos actuales
+    print("Recreando todas las tablas...")
+    Base.metadata.create_all(bind=engine)
+    print("Base de datos reiniciada.")
