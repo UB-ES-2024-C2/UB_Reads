8d3eeb647174b89d7736e2709ca22938b7e15065
Johnny Astudillo Soriano <johnnyastudillosoriano@Johnnys-Air.technicolor.net>
2024-11-26 20:07:53 +0100

    Functions for add button created

--- UBReads_frontend/src/components/views/Book.jsx
+++ UBReads_frontend/src/components/views/Book.jsx
@@ -1,12 +1,17 @@
 import './book.css';
 import React from 'react';
 
+import getUserData from '../../services/getData.js';
+import LibraryService from '../../services/LibraryService.js';
+
 // Material UI imports
 import StarIcon from '@mui/icons-material/Star'; // Icons
 import { green, blue } from '@mui/material/colors'; // Colors
 import { Typography, Button, Rating, Box } from '@mui/material'; // Components
 
 export const Book = ({ book }) => {
+
+    const [bookAdded, setBookAdded] = React.useState(false);
 
     const labels = {
         0: "0",
@@ -32,7 +37,35 @@ export const Book = ({ book }) => {
         setReadMorePressed(!readMorePressed);
     }
 
-    console.log(book);
+    const addBookToUser = () => {
+        const token = localStorage.getItem("access_token");
+        console.log(book);
+        getUserData.getUserData(token).then((user) => {
+            LibraryService.addBookToUser(user.id, book.id).then(() => {
+                setBookAdded(true);
+            });
+        });
+    }
+
+    const removeBookFromUser = () => {
+        const token = localStorage.getItem("access_token");
+        getUserData.getUserData(token).then((user) => {
+            LibraryService.removeBookFromUser(user.id, book.id).then(() => {
+                setBookAdded(false);
+            });
+        });
+    }
+
+    React.useEffect(() => {
+        const token = localStorage.getItem("access_token");
+        getUserData.getUserData(token).then((user) => {
+            LibraryService.getBooksByUser(user.id).then((books) => {
+                books.data.forEach((b2) => {
+                    if (b2.id === book.id) setBookAdded(true);
+                });
+            });
+        });
+    }, []);
 
     return (
         <div id="book-container">
@@ -57,17 +90,19 @@ export const Book = ({ book }) => {
                     )}
                 </div>
                 <Button
-                        variant="contained"
-                        sx={{
-                            bgcolor: green['A700'],
-                            paddingInline: '3rem',
-                            textTransform: 'capitalize',
-                            fontSize: '1.2rem',
-                            borderRadius: '0.5rem'
-                        }}
-                        >
-                            Afegir
-                        </Button>
+                    id="add-button"
+                    variant="contained"
+                    sx={{
+                        bgcolor: green['A700'],
+                        paddingInline: '3rem',
+                        textTransform: 'capitalize',
+                        fontSize: '1.2rem',
+                        borderRadius: '0.5rem'
+                    }}
+                    onClick={bookAdded ? removeBookFromUser : addBookToUser}
+                    >
+                        {bookAdded ? 'Eliminar' : 'Afegir'}
+                </Button>
                 {book.desciption !== undefined && (
                     <div id="content-container">
                         <Typography variant="h5" component="p" sx={{ maxHeight: readMorePressed ? 'none' : '2lh', overflow: 'hidden' }}>{ book.desciption }</Typography>
