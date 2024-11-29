3966c56bcc2cde17fc1b695280eb5107a2b39da5
Johnny Astudillo <56298298+neorefraction@users.noreply.github.com>
2024-11-29 19:19:44 +0100

    Revert "Revert 114 sprint2/us12/frontend/feature/afegir checkbox a vista llibre"

--- UBReads_frontend/src/App.jsx
+++ UBReads_frontend/src/App.jsx
@@ -9,15 +9,33 @@ import { Profile } from "./userProfile";
 
 // BrowserRouter import
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from './components/ProtectedRoute'
 
 // React Component
 const App = () => {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignupForm />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/userProfile" element={<Profile />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/userProfile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
