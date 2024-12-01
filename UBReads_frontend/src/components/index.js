59ef2ae7250dd4eea7f1c6e2d70138e594b094ec
Johnny Astudillo Soriano <johnnyastudillosoriano@Johnnys-MacBook-Air.local>
2024-12-01 11:18:44 +0100

    index file added to components to ease imports

---
+++ UBReads_frontend/src/components/index.js
@@ -0,0 +1,16 @@
+// Page components
+export { Login } from './pages/Login';
+export { Signup } from './pages/Signup';
+export { Home } from './pages/Home';
+
+// View components
+export { Book } from './views/Book';
+export { Search } from './views/Search';
+export { Library } from './views/Library';
+export { Profile } from './views/Profile';
+
+// Layout components
+export { Navbar } from './layout/Navbar';
+
+// Common components
+export { BookRating } from './common/BookRating';
