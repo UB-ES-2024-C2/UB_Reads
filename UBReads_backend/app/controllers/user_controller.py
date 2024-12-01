ef5ccad88dbcc4ec74bf89ea141d172c16d79aff
Andrei Demeter <andreidemeter@Air-de-Andrei-2.home>
2024-12-01 17:29:35 +0100

    Cambis per poder afegir followers by username

--- UBReads_backend/app/controllers/user_controller.py
+++ UBReads_backend/app/controllers/user_controller.py
@@ -86,7 +86,7 @@ class UserController:
         return encoded_jwt
     
     @staticmethod
-    def follow_user(db: Session, user_id: int, to_follow_id: int):
+    def follow_user_with_id(db: Session, user_id: int, to_follow_id: int):
         user = db.query(User).filter(User.id == user_id).first()
         to_follow = db.query(User).filter(User.id == to_follow_id).first()
 
@@ -96,6 +96,22 @@ class UserController:
         if to_follow in user.following:
             raise ValueError("User is already following this target")
 
+        user.following.append(to_follow)
+        db.commit()
+
+    @staticmethod
+    def follow_user_with_username(db: Session, username: str, to_follow_username: str):
+        # Buscar al usuario actual y al objetivo por username
+        user = db.query(User).filter(User.username == username).first()
+        to_follow = db.query(User).filter(User.username == to_follow_username).first()
+
+        if not user or not to_follow:
+            raise ValueError("User or target to follow not found")
+
+        if to_follow in user.following:
+            raise ValueError("User is already following this target")
+
+        # Agregar el usuario objetivo a la lista de "following"
         user.following.append(to_follow)
         db.commit()
 
