514ad778fffebf80b96719d7a797d268a2022df1
Johnny Astudillo Soriano <johnnyastudillosoriano@Johnnys-MacBook-Air.local>
2024-12-01 11:25:31 +0100

    Login and Signup components renamed to ease the understandig

--- UBReads_frontend/src/components/pages/signup.jsx
+++ UBReads_frontend/src/components/pages/signup.jsx
@@ -1,10 +1,10 @@
 import "./styles/signup.css";
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import { signup } from "./utils/UserServices.js"
+import { signup } from "../../utils/UserServices.js"
 import { Link, useNavigate } from "react-router-dom";
 
-export const SignupForm = () => {
+export const Signup = () => {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
