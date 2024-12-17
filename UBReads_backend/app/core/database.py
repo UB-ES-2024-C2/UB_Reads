15aa4a7999ebc1f5817c5a828de975b67197bc42
Andrei Demeter <andreidemeter@Air-de-Andrei-2.home>
2024-12-17 12:13:47 +0100

    Fix CD/CI

--- UBReads_backend/app/core/database.py
+++ UBReads_backend/app/core/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-SQL_DATABASE_URL = "sqlite:///./database.db"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT", 5432)
 
-engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False})
+SQL_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
+engine = create_engine(SQL_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
