71d2ee6667174c508c3200e2dc20ca7cdc4e1adc
Andrei Demeter <andreidemeter@Air-de-Andrei-2.home>
2024-12-17 12:42:08 +0100

    Fix CD/CI-6

--- UBReads_backend/app/controllers/user_controller.py
+++ UBReads_backend/app/controllers/user_controller.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = os.environ.get("ALGORITHM")
+SECRET_KEY = os.environ.get("SECRET_KEY","my_secret")
+ALGORITHM = os.environ.get("ALGORITHM","HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.environ.get("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 
--- UBReads_backend/app/core/database.py
+++ UBReads_backend/app/core/database.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT", 5432)
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD","postgres")
+DB_HOST = os.getenv("DB_HOST","db",)
+DB_NAME = os.getenv("DB_NAME", "ubreads_db")
+DB_PORT = os.getenv("DB_PORT", 5432)  
 
 SQL_DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
